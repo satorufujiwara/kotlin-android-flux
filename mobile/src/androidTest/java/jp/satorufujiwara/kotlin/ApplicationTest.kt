0a0a06ed8b549fa726c1e0c6e9a0ab549ea9ab19
satorufujiwara <holly.wist@gmail.com>
2015-09-25 14:45:05 +0900

    Convert to Kotlin.

---
+++ mobile/src/androidTest/java/jp/satorufujiwara/kotlin/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package jp.satorufujiwara.kotlin
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+public class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
